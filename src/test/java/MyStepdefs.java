4b9d8a7ca7da9b76dd2ec352df0fcdb8531b6021
joaki <joakim_holmstrom8@hotmail.com>
2026-01-03 15:40:25 +0100

    Test

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,128 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class MyStepdefs {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String testEmail = "yasidi2579+" + System.currentTimeMillis() + "@emaxasp.com";
+
+
+    @Given("I am on basketballengland.co.uk on {string}")
+    public void iAmOnBasketballenglandCoUkOn(String browser) {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    System.out.println("Chrome driver loaded");
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    System.out.println("Firefox driver loaded");
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    System.out.println("Edge driver loaded");
+                    break;
+            }
+            wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+            driver.get("C:\\Users\\joaki\\Downloads\\Register\\Register.html");
+
+    }
+
+    @When("I fill in date of birth {string}")
+    public void iFillInDateOfBirth(String dateOfBirth) {
+        driver.findElement(By.cssSelector("input#dp")).sendKeys(dateOfBirth);
+
+    }
+
+    @And("I fill in the first name {string}")
+    public void iFillInTheFirstName(String firstname) {
+        driver.findElement(By.cssSelector("input#member_firstname")).sendKeys(firstname);
+    }
+
+    @And("I fill in an last name {string}")
+    public void iFillInAnLastName(String lastname) {
+        driver.findElement(By.cssSelector("input#member_lastname")).sendKeys(lastname);
+    }
+
+    @And("I fill in the email and then confirm email")
+    public void iFillInTheEmailAndThenConfirmEmail() {
+        driver.findElement(By.cssSelector("input#member_emailaddress")).sendKeys(testEmail);
+        driver.findElement(By.cssSelector("input#member_confirmemailaddress")).sendKeys(testEmail);
+
+    }
+
+    @And("I choose the password and retype the password {string}")
+    public void iChooseThePasswordAndRetypeThePassword(String password) {
+        driver.findElement(By.cssSelector("input#signupunlicenced_password")).sendKeys(password);
+        driver.findElement(By.cssSelector("input#signupunlicenced_confirmpassword")).sendKeys(password);
+    }
+
+
+    @And("I check I have read Terms and Conditions {string}")
+    public void iCheckIHaveReadTermsAndConditions(String terms) {
+        if (terms.trim().equalsIgnoreCase("true"))
+            driver.findElement(By.cssSelector("label[for='sign_up_25'] .box")).click();
+    }
+
+
+    @And("I confirm that I am over the age limit")
+    public void iConfirmThatIAmOverTheAgeLimit() {
+        WebElement confirm = (new WebDriverWait(driver, Duration.ofSeconds(3))).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("label[for='sign_up_26'] .box")));
+        confirm.click();
+    }
+
+    @And("I check that I have read Code of Conduct")
+    public void iCheckThatIHaveReadCodeOfConduct() {
+        driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct'] .box")).click();
+    }
+
+    @And("Then I press Confirm and join")
+    public void thenIPressConfirmAndJoin() {
+        driver.findElement(By.cssSelector(".btn")).click();
+    }
+
+    @Then("I successfully become a member")
+    public void iSuccessfullyBecomeAMember() {
+        waitToBeDisplayed("h2.gray");
+        String expected = "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND";
+        String actual = driver.findElement(By.cssSelector("h2.gray")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @And("I fill in password {string}")
+    public void iFillInPassword(String password) {
+        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys(password);
+    }
+
+    @And("I fill in confirmed password {string}")
+    public void iFillInConfirmedPassword(String confirmPassword) {
+        driver.findElement(By.cssSelector("input#signupunlicenced_confirmpassword")).sendKeys(confirmPassword);
+        driver.findElement(By.cssSelector("#titleText1")).click();
+    }
+
+    @Then("It failed because {string}")
+    public void itFailBecause(String expectedFailureMessage) {
+        WebElement element = driver.findElement(By.cssSelector("span[generated=\"true\"]"));
+        String actual = element.getText();
+        assertEquals(expectedFailureMessage, actual);
+        System.out.println("Misslyckades på grund av: " + actual);
+    }
+
+    private void waitToBeDisplayed(String css) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(css)));
+    }
+
+}
---
+++ src/test/resources/features/basketBallClub.feature
@@ -0,0 +1,39 @@
+Feature:Test of regristration of users
+
+  Scenario Outline: Add new user and everything should work.
+    Given I am on basketballengland.co.uk on "<browser>"
+    When I fill in date of birth "<date of birth>"
+    And I fill in the first name "<first name>"
+    And I fill in an last name "<last name>"
+    And I fill in the email and then confirm email
+    And I choose the password and retype the password "<password>"
+    And I check I have read Terms and Conditions "<terms accepted>"
+    And I confirm that I am over the age limit
+    And I check that I have read Code of Conduct
+    And Then I press Confirm and join
+    Then I successfully become a member
+    Examples:
+      | browser | date of birth | first name | last name | password | terms accepted |
+      | chrome  |05/05/1996 | Jocke | Holmstrom | test12345 | true |
+      | firefox  |06/02/1990 | Henrik | Stormare | test55221 | true |
+      | edge  |15/08/1998 | Daniel | Andersson | testsss15 | true |
+
+  Scenario Outline: Add new user and try to fail.
+    Given I am on basketballengland.co.uk on "<browser>"
+    When I fill in date of birth "<date of birth>"
+    And I fill in the first name "<first name>"
+    And I fill in an last name "<last name>"
+    And I fill in the email and then confirm email
+    And I fill in password "<password>"
+    And I fill in confirmed password "<confirmed password>"
+    And I check I have read Terms and Conditions "<terms accepted>"
+    And I confirm that I am over the age limit
+    And I check that I have read Code of Conduct
+    And Then I press Confirm and join
+    Then It failed because "<error message>"
+
+    Examples:
+      | browser | date of birth | first name | last name | password | confirmed password | terms accepted | error message |
+      | chrome  |05/05/1996 | Jocke |  | test12345 | test12345 | true | Last Name is required |
+      | firefox  |01/05/1992 | Henrik | Stormare | test12345 | test1234 | true | Password did not match |
+      | chrome  |15/08/1998 | Daniel | Andersson | testsss15 |testsss15 | false | You must confirm that you have read and accepted our Terms and Conditions |
